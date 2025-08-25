82ac1557a7e618208dac0cc3f535c77aeffb2f6b
Saiful Alom <saif.zsdb2gmail.com>
2025-08-25 16:34:56 +0600

    unnesary text removed

--- src/components/header/Navigaion.jsx
+++ src/components/header/Navigaion.jsx
@@ -5,7 +5,7 @@ export default function Navigation() {
   return (
     <div className='flex gap-3 py-5 px-1 bg-foreground/10 backdrop-blur-sm shadow-sm rounded-full items-center'>
         {navigationLinks.map(({ Icon , name})=> (
-            <div key={name} className='flex text-sm gap-3 cursor-pointer bg-foreground/10 backdrop-blur-lg rounded-full py-1 px-2 items-center'>
+            <div key={name} className='flex text-sm gap-1 cursor-pointer bg-foreground/10 backdrop-blur-lg rounded-full py-1 px-2 items-center'>
                <Icon className='w-4'/> <span className='text-xs'>{name}</span>
             </div>
         ))}
