9adccd83181242015aba74b12692eb863d4e0ad1
Saiful Alom <saif.zsdb2gmail.com>
2025-09-07 15:34:44 +0600

    project seted-up

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div>
+      <Button>This is test</Button>
+    </div>
+  )
+}
