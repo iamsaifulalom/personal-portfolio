11b43b2cf4bb7ac0d2f85079e2b49b0187ea125d
Saiful Alom <saif.zsdb2gmail.com>
2025-09-07 16:37:14 +0600

    css configured

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+        <h1 className='text-9xl font-Spectral'>This is her</h1>
+    </div>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { AnimatePresence } from 'framer-motion'
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AnimatePresence mode='wait'>
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
 }
