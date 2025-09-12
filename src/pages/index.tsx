3650b03ea8248dc91ef3985663454a57a82ccb53
Saiful Alom <saif.zsdb2gmail.com>
2025-09-12 21:03:37 +0600

    indes page added

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div>
+      <h1 className='text-2xl font-AnastasiaScript'>Lorem ipsum dolor sit.</h1>
+      <h1 className='text-2xl font-Manrope'>Lorem ipsum dolor sit.</h1>
+    </div>
+  )
+}
